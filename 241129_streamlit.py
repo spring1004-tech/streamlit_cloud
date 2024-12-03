9d5ad596f14d629856d681607c59f34cf908630d
spring <spring3513@gmail.com>
2024-12-03 17:10:43 +0900

    spring

--- 241129_streamlit.py
+++ 241129_streamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from mistralai import Mistral, UserMessage
 
+
 st.title("첫 스트림릿 예제")
 st.write('안녕하세요, 처음 만드는 스트림릿 예제입니다.')
 
@@ -30,3 +31,21 @@ st.slider("슬라이더를 테스트합니다.", 0, 100, 50)
 st.button("버튼을 눌러보세요.")
 st.checkbox("체크박스를 테스트합니다.")
 st.radio("라디오 버튼을 테스트합니다.", ["옵션1", "옵션2", "옵션3"])
+import os
+
+secret_key = os.getenv("MISTRAL_API_KEY")
+client = Mistral(api_key=secret_key)
+model = "mistral-large-latest"
+
+# 채팅 메시지 준비
+messages = [
+    {"role": "user",
+     "content": "파이썬은 정말 재미있는 언어야. 를 영어로 번역해줘."},
+]
+
+# API 호출
+chat_response = client.chat.complete(
+    model=model,
+    messages=messages
+)
+st.write(chat_response.choices[0].message.content)
